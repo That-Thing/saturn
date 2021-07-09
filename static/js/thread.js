824908ce5783793635aa6e07fe24b4b770c8bfb6
Sen <sen@onee.ch>
2021-07-09 14:01:52 -0400

    minor color change to the tomorrow theme. Better handling of marked posts (there can only be one marked post at a time)

--- static/js/thread.js
+++ static/js/thread.js
@@ -1,8 +1,15 @@
 //mark post linked in url
-var splitUrl = window.location.href.split('#');
-if (splitUrl.length > 1) {
-    if(document.getElementById(splitUrl[1]) != null) {
-        document.getElementById(splitUrl[1]).childNodes[1].className += " marked-post";
+function markPost() {
+    var elements = document.getElementsByClassName('marked-post');
+    while(elements.length > 0){
+        elements[0].classList.remove('marked-post');
+    }
+    var splitUrl = window.location.href.split('#');
+    if (splitUrl.length > 1) {
+        if(document.getElementById(splitUrl[1]) != null) {
+            document.getElementById(splitUrl[1]).childNodes[1].className += " marked-post";
+        }
     }
 }
-
+markPost();
+window.addEventListener("hashchange", markPost);
