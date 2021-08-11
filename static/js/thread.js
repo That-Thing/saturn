4733d47010b708a3e2aaf9ac1ae3415b090dc1ca
Sen <sen@onee.ch>
2021-08-11 13:46:09 -0400

    fixed and perfected replies.

--- static/js/thread.js
+++ static/js/thread.js
@@ -1,4 +1,3 @@
-//mark post linked in url
 function checkUrl() {
     var elements = document.getElementsByClassName('marked-post');
     while(elements.length > 0){
@@ -6,18 +5,19 @@ function checkUrl() {
     }
     var splitUrl = window.location.href.split('#');
     if (splitUrl.length > 1) {
-        if(splitUrl[1].charAt(0) == "q") {
-            console.log(">>"+splitUrl[1].substring(1))
-            document.getElementById("message").innerHTML = document.getElementById("message").innerHTML + ">>"+splitUrl[1].substring(1)+"\n";
-        } else if(document.getElementById(splitUrl[1]) != null) {
+        if(document.getElementById(splitUrl[1]) != null) {
             document.getElementById(splitUrl[1]).childNodes[1].className += " marked-post";
         }
     }
 }
 checkUrl();
 window.addEventListener("hashchange", checkUrl);
-
-//add reply to textarea
-// function addReply(reply) {
-//     document.getElementById("message").innerHTML = document.getElementById("message").innerHTML + ">>" + "\n"
-// }
+function reply(post) {
+    document.getElementById("comment").innerHTML += ">>" + post.text + "\n";
+}
+window.onload = function() {
+    var splitUrl = window.location.href.split('#');
+    if (splitUrl[1] && splitUrl[1].charAt(0) == "q") {
+        document.getElementById("comment").innerHTML += ">>" + splitUrl[1].substring(1) + "\n";
+    }
+}
